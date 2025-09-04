5c8d915bfc0431950fc56867f2f8db23d389d101
Veliz1232 <cr.veliz@duocuc.cl>
2025-09-04 15:06:06 -0400

    pene

---
+++ blogshtml/funciones.js
@@ -0,0 +1,7 @@
+document.getElementById('1').addEventListener('click', function() {
+    window.location.href = 'Caso1.html';
+});
+
+document.getElementById('2').addEventListener('click', function() {
+    window.location.href = 'Caso2.html';
+});
